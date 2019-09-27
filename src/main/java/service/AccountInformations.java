60a93500572c8c6875cf42c7d707b66f1ef3914f
FabriceGh <fabrice.ghysdael@gmail.com>
2019-09-27 11:42:59 +0200

    Added statement filters via AccountInformations service%.

---
+++ src/main/java/service/AccountInformations.java
@@ -0,0 +1,27 @@
+package service;
+
+import domain.Account;
+import domain.Statement;
+import domain.StatementType;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AccountInformations {
+
+    public static List<Statement> getStatementsFilteredByType(Account account, List<StatementType> filterTypes){
+
+        return account.getStatements().stream().filter(statement -> filterTypes.contains(statement.getType()))
+                .collect(Collectors.toList());
+
+    }
+
+    public static List<Statement> getStatementsFilteredByDate(Account account, LocalDate dateFilter){
+
+        return account.getStatements().stream().filter(statement -> statement.getDate().equals(dateFilter))
+                .collect(Collectors.toList());
+
+    }
+
+}
