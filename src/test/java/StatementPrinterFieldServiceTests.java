154314c8286e90c02d18144715a783a16e2c0c31
FabriceGh <fabrice.ghysdael@gmail.com>
2019-10-01 09:52:32 +0200

    Refacto Statement printer service class ; isolate field.

--- src/test/java/StatementPrinterFieldServiceTests.java
+++ src/test/java/StatementPrinterFieldServiceTests.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import service.StatementPrinterFieldService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatementPrinterFieldServiceTests {
+
+
+
+    @Test
+    void should_return_padded_field(){
+        String paddedField;
+        paddedField = StatementPrinterFieldService.fieldSpacePadded("test", 10);
+
+        assertEquals("      test", paddedField);
+
+    }
+
 }
